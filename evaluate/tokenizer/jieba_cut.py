9b24b5e70e53f867fd0f86cee6f0b5f38640ae1b
Ryan Chao <ryanchao2012@gmail.com>
2017-09-02 13:15:36 +0000

    fix jieba_cut.py newline bug

--- evaluate/tokenizer/jieba_cut.py
+++ evaluate/tokenizer/jieba_cut.py
@@ -3,5 +3,5 @@ import fileinput
 
 
 for line in fileinput.input():
-    print(' '.join([tok.word for tok in pseg.cut(line)]))
+    print(' '.join([tok.word for tok in pseg.cut(line) if bool(tok.word.strip())]))
 
