61abb9acce74bf86765b6ef3672ec55eccfc38af
Ryan Chao <ryanchao2012@gmail.com>
2017-09-06 14:57:54 +0000

    regular save

--- slackbot/okcomputer.py
+++ slackbot/okcomputer.py
@@ -8,7 +8,7 @@ from slackbot.bot import Bot
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
 
-from bots import MessengerBot
+# from bots import MessengerBot
 
 config_parser = RawConfigParser()
 config_parser.read('slackbot_config.ini')
@@ -17,7 +17,8 @@ bot.settings.API_TOKEN  = config_parser.get('okcomputer', 'token')
 
 
 def algorithm(string):
-    reply = MessengerBot(string, 'messenger', 'slack').retrieve()
+    # reply = MessengerBot(string, 'messenger', 'slack').retrieve()
+    reply = '嘻嘻'
     return string + ', ' + reply
 
 
